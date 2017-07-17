fed2ceda90ac9d7f181d3f364b0ee48f8b5eb12b
Herr doctor Pep <HerrDoctorPep@users.noreply.github.com>
2017-07-17 21:18:44 +0200

    Adaptive screen added
    Scale canvas, orbits and body sizes

--- DefineCanvas.js
+++ DefineCanvas.js
@@ -1,12 +1,14 @@
 // define the drawing context
 var ctx = myCanvas.getContext("2d");
+var cwdth = window.innerWidth;
+var chth = window.innerHeight;
 
 // draw a body in a color
 function DrawBody(Planet,ColorCode) {
   var rect = myCanvas.getBoundingClientRect();
   ctx.fillStyle = Planet.colorcode;
   ctx.beginPath();
-  ctx.arc(MeterToPixel * Planet.place.x - rect.left, MeterToPixel * Planet.place.y - rect.top, Math.log(Planet.mass)/5, 0, Math.PI * 2);
+  ctx.arc(MeterToPixel * Planet.place.x - rect.left, MeterToPixel * Planet.place.y - rect.top, (Math.log(Planet.mass)/Math.log(Sun.mass))*(Math.min(MidX,MidY)/20), 0, Math.PI * 2);
   ctx.fill();
 }
 
